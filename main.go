ecf09790f20daef40e81b028d28a1943212fd303
whxcer <whxcer@outlook.com>
2023-08-29 23:05:53 +0800

    gin渲染-HTML渲染

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
     "github.com/gin-gonic/gin"
+    "net/http"
 )
 
 func main() {
     r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
+    r.GET("/", func(c *gin.Context) {
         c.JSON(200, gin.H{
             "message": "hello world",
         })
     })
+
+    r.LoadHTMLGlob("templates/*.html")
+    r.GET("/index", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.html", gin.H{
+            "title": "index",
+            "test": "test",
+        })
+    })
+
     r.Run()
 }
