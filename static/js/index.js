c7fc86e8c682dc979b537ebd7d30e48109e9341c
whxcer <whxcer@outlook.com>
2023-08-30 23:33:38 +0800

    前端代码的修改：去除jquery，使用原生的JavaScript && todo任务的添加

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,25 @@
-$(document).ready(function() {
+(function() {
+    var $ = function(selector) {
+        return document.querySelector(selector)
+    }
+
+    var bindEventAdd = function() {
+        // 给 add button 绑定点击事件
+        $('.todo-add').addEventListener('click', function() {
+            let value = $('.todo-input').value
+            console.log("value = ", value);
+        })
+    }
+
+    var bindEvents = function() {
+        // 添加 todo
+        bindEventAdd()
+    }
+
     var __main = function() {
-        $('#header').html(`<h1>备忘录</h1>`)
+        // 绑定事件
+        bindEvents()
     }
 
     __main()
-});
+})();
