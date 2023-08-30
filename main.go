d057e4294bb65bd8c09fb964d180ce6424d681a7
whxcer <whxcer@outlook.com>
2023-08-30 23:18:31 +0800

    服务器添加数据的格式 && 客户端获取数据

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
     "net/http"
 )
 
+// 定义结构体 NotesFormat
+type NotesFormat struct {
+    ID int
+    Notes string
+}
+
 func main() {
     // 创建一个默认的 Gin 路由引擎实例
     // 默认路由引擎实例已经预先配置了一些常用的中间件，例如日志记录和错误恢复中间件
@@ -19,6 +25,16 @@ func main() {
     // 注册首页的路由
     r.GET("/", func(c *gin.Context) {
         c.HTML(http.StatusOK, "index.html", gin.H{})
+    })
+
+    // 创建 NotesFormat 类型的切片
+    var todolist_data []NotesFormat
+    // 添加元素到切片
+    todolist_data = append(todolist_data, NotesFormat{ID: 1, Notes: "Hello"})
+    todolist_data = append(todolist_data, NotesFormat{ID: 2, Notes: "World"})
+
+    r.GET("/todolist", func(c *gin.Context) {
+        c.JSON(http.StatusOK, todolist_data)
     })
 
     // 启动服务器，默认在 0.0.0.0:8080 启动服务
